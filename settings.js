9515f3110811f82c4c65c0deab583c1f04d7fb11
PannBOT <108059124+PannBOT@users.noreply.github.com>
2022-08-16 04:29:26 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,7 +13,7 @@ global.APIKeys = {
 }
 
 // setting 
-global.autoread = true // auto read pesan / message
+global.autoread = false // auto read pesan / message
 global.autobio = true 
 
 //language
